c8b7fe5f4504aa8ce8988d69ee845667f12a13c0
Lusiek <blacktentdev@outlook.com>
2023-03-06 15:48:32 +0100

    Initial Commit

---
+++ Parade/ParadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParadeApp.swift
+//  Parade
+//
+//  Created by Łukasz Szymczuk on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
